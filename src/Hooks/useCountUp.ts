2af92b7eaeeba96c2a9bfbf67fa42d1d9edefc1e
HyeokE <jhjeong00@gmail.com>
2022-06-18 15:03:05 +0900

    refactor: useCountup setInterval -> RAF로 변경

--- src/Hooks/useCountUp.ts
+++ src/Hooks/useCountUp.ts
@@ -1,30 +1,42 @@
 import { useEffect, useState } from "react";
 
-function easeOut(t: number) {
-  return t === 1 ? 1 : -Math.pow(2, -14 * t) + 1;
-}
+type EaseOutExpoType = (timeDuration: number, duration: number) => number;
 
-const DURATION = 2000;
-const FRAME_RATE = 1000 / 60;
+type UseCountUpType = (
+  num: number,
+  duration?: number,
+  trigger?: boolean
+) => number;
 
-const useCountUp = (num: number) => {
+const easeOutExpo: EaseOutExpoType = (timeDuration, duration) => {
+  return 1 - Math.pow(2, (-11 * timeDuration) / duration);
+};
+
+const useCountUp: UseCountUpType = (num, duration = 2000, trigger = true) => {
   const [count, setCount] = useState(0);
+  let frameRef = 0;
+  let animationId;
 
+  const getNextNumber = (timestamp: number) => {
+    const result = Number(easeOutExpo(timestamp, duration).toFixed(3));
+    return Math.floor(result * num);
+  };
+
+  const countUpNumberAnimation = (timestamp: number) => {
+    const next = getNextNumber(timestamp);
+    setCount(next);
+    frameRef = next;
+    animationId = requestAnimationFrame(countUpNumberAnimation);
+    if (timestamp >= duration) {
+      frameRef = num;
+      setCount(num);
+    }
+    if (frameRef >= num) {
+      cancelAnimationFrame(animationId);
+    }
+  };
   useEffect(() => {
-    const totalFrame = Math.round(DURATION / FRAME_RATE);
-    let frame = 0;
-    const counter = setInterval(() => {
-      frame++;
-      const progress = Number(easeOut(frame / totalFrame).toFixed(3));
-      const currentNumber = Math.floor(num * progress);
-      if (num + 1 > currentNumber) {
-        setCount(currentNumber);
-      }
-      if (FRAME_RATE * 120 === frame || frame === totalFrame) {
-        setCount(num);
-        clearInterval(counter);
-      }
-    }, FRAME_RATE);
+    trigger && requestAnimationFrame(countUpNumberAnimation);
   }, [num]);
   return count;
 };
